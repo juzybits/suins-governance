105953ebbd5d2c74337803657cc8b5a6d5e12e09
Godwin Jibs <g.jibuike@mystenlabs.com>
2024-11-20 13:52:07 -0600

    nit

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,3 +1,4 @@
+import { proposalRouter } from "@/server/api/routers/proposal";
 import { createCallerFactory, createTRPCRouter } from "@/server/api/trpc";
 
 /**
@@ -5,7 +6,9 @@ import { createCallerFactory, createTRPCRouter } from "@/server/api/trpc";
  *
  * All routers added in /api/routers should be manually added here.
  */
-export const appRouter = createTRPCRouter({});
+export const appRouter = createTRPCRouter({
+  post: proposalRouter,
+});
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
