13092fc81d14e7b0ee470a63beeccc831e47e7c0
juzybits <96890594+juzybits@users.noreply.github.com>
2025-04-11 19:55:46 +0200

    add const V1_PROPOSAL_IDS

--- src/hooks/useGetProposals.ts
+++ src/hooks/useGetProposals.ts
@@ -32,11 +32,19 @@ export function useGetProposalsIds() {
       const governanceData = governanceSchema.safeParse(
         dynamicFieldsObject.data?.content,
       );
-      return governanceData;
+
+      if (governanceData.error)
+        return [];
+
+      const proposals = governanceData.data?.fields.value.fields.pos0;
+      if (NETWORK === "mainnet") {
+        proposals.push(...V1_PROPOSAL_IDS);
+      }
+
+      return proposals;
     },
-    select: (governanceData) => {
-      if (governanceData.error) return null;
-      const sortedProposals = governanceData.data?.fields.value.fields.pos0
+    select: (proposals) => {
+      const sortedProposals = proposals
         .sort(
           (b, a) => parseInt(a.fields.end_time) - parseInt(b.fields.end_time),
         )
@@ -48,3 +56,30 @@ export function useGetProposalsIds() {
     },
   });
 }
+
+const V1_PROPOSAL_IDS = [
+  // #1: SuiNS DAO Constitution
+  {
+    "type": "0xd43eeaa30cb62d94ecf7d2a2283913486bfd9288926f9f7ff237ac7a6cb44b41::early_voting::ProposalPointer",
+    "fields": {
+      "end_time": "1732816800000",
+      "proposal_id": "0xd4c794821436f03f1dc8321f4939c17d67c54aa6661a7b8dee12b3d179601001"
+    }
+  },
+  // #2: SuiNS Pricing & Renewal Rates
+  {
+    "type": "0xd43eeaa30cb62d94ecf7d2a2283913486bfd9288926f9f7ff237ac7a6cb44b41::early_voting::ProposalPointer",
+    "fields": {
+      "end_time": "1734811200000",
+      "proposal_id": "0x8e71b81bd327fc8ae721518ccbfc0964fe19c8300773307eb6f0746e7f620b8f"
+    }
+  },
+  // #3: SuiNS RFP Selection
+  {
+    "type": "0xd43eeaa30cb62d94ecf7d2a2283913486bfd9288926f9f7ff237ac7a6cb44b41::early_voting::ProposalPointer",
+    "fields": {
+      "end_time": "1740175200000",
+      "proposal_id": "0x9ee1af58d737418649101836c8ba926cbe28e522e22e315dd7bd649c94425a0b"
+    }
+  },
+];
