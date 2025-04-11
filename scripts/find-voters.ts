0df802626efbb9fa566be782e61ba1e31508e9e9
juzybits <96890594+juzybits@users.noreply.github.com>
2025-04-11 22:35:14 +0200

    find-voters: parse events

--- scripts/find-voters.ts
+++ scripts/find-voters.ts
@@ -1,44 +1,76 @@
 import { getFullnodeUrl, SuiClient } from "@mysten/sui/client";
+import { formatNSBalance } from "../src/utils/formatNumber";
 
 const governancePkgV1 = "0xd43eeaa30cb62d94ecf7d2a2283913486bfd9288926f9f7ff237ac7a6cb44b41";
+const eventsPerQuery = 1;
 
 const main = async () => {
     const client = new SuiClient({ url: getFullnodeUrl("mainnet") });
-    console.log(JSON.stringify(await queryTxs(client), null, 2));
+    // console.log(JSON.stringify(await queryTxs(client), null, 2));
+    console.log(await queryTxs(client));
     // console.log(JSON.stringify(await queryEvents(client), null, 2));
 };
 
-async function queryTxs(client: SuiClient) {
-    const txs = await client.queryTransactionBlocks({
-        filter: {
-            MoveFunction: {
-                package: governancePkgV1,
-                module: "proposal",
-                function: "return_tokens_bulk",
-            },
-        },
-        options: {
-            // showBalanceChanges: true,
-            // showEffects: true,
-            showEvents: true,
-            // showInput: true,
-            // showObjectChanges: true,
-            // showRawEffects: true,
-            // showRawInput: true,
-        },
-        limit: 1,
-    });
-    return txs;
+type ReturnTokenEvent = {
+    tx_digest: string;
+    timestamp: string;
+    voter_addr: string;
+    amount_raw: string;
+    amount_ns: string;
 }
 
-async function queryEvents(client: SuiClient) {
-    const events = await client.queryEvents({
-        query: {
-            MoveEventType: `${governancePkgV1}::proposal::ReturnTokenEvent`,
-        },
-        limit: 1,
-    });
+async function queryTxs(client: SuiClient) {
+    let events: ReturnTokenEvent[] = [];
+    let hasNextPage = true;
+    let cursor: string | null | undefined = null;
+    while (hasNextPage) {
+        const txs = await client.queryTransactionBlocks({
+            cursor,
+            limit: eventsPerQuery,
+            filter: {
+                MoveFunction: {
+                    package: governancePkgV1,
+                    module: "proposal",
+                    function: "return_tokens_bulk",
+                },
+            },
+            options: {
+                // showBalanceChanges: true,
+                // showEffects: true,
+                showEvents: true,
+                // showInput: true,
+                // showObjectChanges: true,
+                // showRawEffects: true,
+                // showRawInput: true,
+            },
+        });
+        hasNextPage = txs.hasNextPage;
+        cursor = txs.nextCursor;
+        for (const tx of txs.data) {
+            for (const event of tx.events!) {
+                const json: any = event.parsedJson;
+                events.push({
+                    tx_digest: tx.digest,
+                    timestamp: tx.timestampMs!,
+                    voter_addr: json.voter,
+                    amount_raw: json.amount,
+                    amount_ns: formatNSBalance(json.amount),
+                })
+            }
+        }
+        break; // TODO: remove
+    }
     return events;
 }
+
+// async function queryEvents(client: SuiClient) {
+//     const events = await client.queryEvents({
+//         query: {
+//             MoveEventType: `${governancePkgV1}::proposal::ReturnTokenEvent`,
+//         },
+//         limit: 1,
+//     });
+//     return events;
+// }
 
 main();
