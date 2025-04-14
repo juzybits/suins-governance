a7c0525261b20e723a044ffc0d2de2a31ab4182a
juzybits <96890594+juzybits@users.noreply.github.com>
2025-04-14 15:49:28 +0200

    scripts: add getRandomAirdropConfig.ts

--- scripts/src/airdrop-staked.ts
+++ scripts/src/airdrop-staked.ts
@@ -1,24 +1,27 @@
 import { readFileSync } from "fs";
-import { SuiClient } from "@mysten/sui/client";
-import type { AirdropConfig } from "./generate-airdrop-config";
-import { SUINS_ENDPOINTS } from "../../src/constants/endpoints";
+import { getFullnodeUrl, SuiClient } from "@mysten/sui/client";
+import { getActiveEnv, getActiveKeypair } from "@polymedia/suitcase-node";
 import { CoinFormat, formatBalance, NS_DECIMALS } from "../../src/utils/formatNumber";
+import type { AirdropConfig } from "./generate-airdrop-config";
 import { promptUser } from "./utils";
+import { getRandomAirdropConfig } from "./getRandomAirdropConfig";
 
 async function main() {
-    const filePath = process.argv[2];
+    // const filePath = process.argv[2];
 
-    if (!filePath) {
-        console.error("Error: provide a JSON file path as an argument.");
-        process.exit(1);
-    }
+    // if (!filePath) {
+    //     console.error("Error: provide a JSON file path as an argument.");
+    //     process.exit(1);
+    // }
 
-    const data = readFileSync(filePath, "utf8");
-    const airdrops: AirdropConfig[] = JSON.parse(data);
+    // const data = readFileSync(filePath, "utf8");
+    // const airdrops: AirdropConfig[] = JSON.parse(data);
 
-    if (!airdrops || !Array.isArray(airdrops) || airdrops.length === 0) {
-        throw new Error("No valid voter data found in the file.");
-    }
+    // if (!airdrops || !Array.isArray(airdrops) || airdrops.length === 0) {
+    //     throw new Error("No valid voter data found in the file.");
+    // }
+
+    const airdrops: AirdropConfig[] = getRandomAirdropConfig(84533);
 
     const proceed = await showSummaryAndConfirm(airdrops);
     if (!proceed) {
@@ -48,7 +51,11 @@ async function executeAirdrop(airdrops: AirdropConfig[]) { // TODO: discuss secu
 
     console.log("Proceeding...");
 
-    const client = new SuiClient({ url: SUINS_ENDPOINTS.mainnet.fullNodes });
+    const [network, signer] = await Promise.all([
+        getActiveEnv(),
+        getActiveKeypair(),
+    ]);
+    const client = new SuiClient({ url: getFullnodeUrl(network) });
 
     console.log(airdrops.slice(0, 5));
 }
