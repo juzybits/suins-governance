48f3c2b666e0f87dea2848c724c1cc3462c3fdfe
juzybits <96890594+juzybits@users.noreply.github.com>
2025-04-16 20:18:10 +0200

    update constants/common.ts

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,10 +1,4 @@
-export const SUI_COIN_OBJECT_TYPE = "0x2::coin::Coin<0x2::sui::SUI>";
-export const NAME_OBJECT_TYPE = "suins_registration::SuinsRegistration";
 export const SUI_TLD = "sui";
-export const SUB_NAME_STRUCT_TYPE =
-  "subdomain_registration::SubDomainRegistration";
 export const NS_DECIMALS = 6;
-export const THREAD_HOLD = 5_000_000;
 export const NS_VOTE_DIVISOR = 1_000_000;
-export const NS_VOTE_THRESHOLD = 5_000_000;
-export const TOTAL_NS = 5_000_000_000_000;
+export const NS_VOTE_THRESHOLD = 1_500_000; // warn: this could change
