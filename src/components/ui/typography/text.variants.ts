61e0ddff3953cee272399c13a1642e76449294e3
Marco <marcomorete73@hotmail.com>
2025-05-14 19:43:25 +0100

    :lipstick: fix: fonts

--- src/components/ui/typography/text.variants.ts
+++ src/components/ui/typography/text.variants.ts
@@ -4,55 +4,56 @@ export const textVariants = cva("", {
   variants: {
     variant: {
       "display/XLarge":
-        "font-highlight text-9xl leading-8xl tracking-tighter font-900",
+        "font-highlight text-9xl leading-8xl tracking-tighter font-[900]",
       "display/Large":
-        "font-highlight text-8xl leading-7xl tracking-tighter font-900",
+        "font-highlight text-8xl leading-7xl tracking-tighter font-[900]",
       "display/Regular":
-        "font-highlight text-7xl leading-6xl tracking-tight font-900",
+        "font-highlight text-7xl leading-6xl tracking-tight font-[900]",
       "display/Small":
-        "font-highlight text-6xl leading-3xl tracking-normal font-900",
+        "font-highlight text-6xl leading-3xl tracking-normal font-[900]",
       "display/XSmall":
-        "font-highlight text-5xl leading-4xl tracking-normal font-900",
+        "font-highlight text-5xl leading-4xl tracking-normal font-[900]",
       "display/XSmall Light":
-        "font-highlight text-5xl leading-4xl tracking-normal font-750",
+        "font-highlight text-5xl leading-4xl tracking-normal font-[750]",
       "display/XXSmall Light":
-        "font-highlight text-2xl leading-xl tracking-normal font-750",
+        "font-highlight text-2xl leading-xl tracking-normal font-[750]",
       "display/XXXSmall Light":
-        "font-highlight text-xl leading-m tracking-normal font-750",
+        "font-highlight text-xl leading-m tracking-normal font-[750]",
       "heading/Large SemiBold":
-        "font-highlight text-5xl leading-xl tracking-normal font-semibold",
+        "font-highlight text-5xl leading-xl tracking-normal font-[600]",
       "heading/Large Medium":
-        "font-highlight text-5xl leading-xl tracking-normal font-medium",
+        "font-highlight text-5xl leading-xl tracking-normal font-[500]",
       "heading/Regular Bold":
-        "font-highlight text-3xl leading-xl tracking-normal font-bold",
+        "font-highlight text-3xl leading-xl tracking-normal font-[700]",
       "heading/Regular Medium":
-        "font-highlight text-3xl leading-xl tracking-normal font-medium",
+        "font-highlight text-3xl leading-xl tracking-normal font-[500]",
       "heading/Regular Regular":
         "font-highlight text-3xl leading-xl tracking-normal",
       "heading/Small Bold":
-        "font-highlight text-2xl leading-2xl tracking-normal font-bold",
+        "font-highlight text-2xl leading-2xl tracking-normal font-[700]",
       "heading/Small Medium":
-        "font-highlight text-2xl leading-2xl tracking-normal font-medium",
+        "font-highlight text-2xl leading-2xl tracking-normal font-[500]",
       "heading/Small Regular":
         "font-highlight text-2xl leading-2xl tracking-normal",
       "heading/XSmall Medium":
-        "font-highlight text-xl leading-xl tracking-normal font-medium",
-      "label/Large Bold": "font-main text-l leading-xs tracking-wide font-bold",
+        "font-highlight text-xl leading-xl tracking-normal font-[500]",
+      "label/Large Bold":
+        "font-main text-l leading-xs tracking-wide font-[700]",
       "label/Large Medium":
-        "font-main text-l leading-xs tracking-wide font-medium",
+        "font-main text-l leading-xs tracking-wide font-[500]",
       "label/Large Regular": "font-main text-l leading-xs tracking-wide",
       "label/Regular Bold":
-        "font-main text-m leading-2xs tracking-wide font-bold",
+        "font-main text-m leading-2xs tracking-wide font-[700]",
       "label/Regular Bold Mono":
-        "font-mono text-m leading-2xs tracking-wide font-bold",
+        "font-mono text-m leading-2xs tracking-wide font-[700]",
       "label/Small Bold":
-        "font-main text-s leading-3xs tracking-wide font-bold",
+        "font-main text-s leading-3xs tracking-wide font-[700]",
       "label/Small Medium":
-        "font-main text-s leading-3xs tracking-wide font-medium",
+        "font-main text-s leading-3xs tracking-wide font-[500]",
       "label/Small Medium Mono":
-        "font-mono text-s leading-3xs tracking-wide font-medium",
+        "font-mono text-s leading-3xs tracking-wide font-[500]",
       "label/XSmall SemiBold":
-        "font-main text-xs leading-4xs tracking-wide font-semibold",
+        "font-main text-xs leading-4xs tracking-wide font-[600]",
       "paragraph/XLarge": "font-main text-4xl leading-5xl tracking-normal",
       "paragraph/Large": "font-main text-l leading-l tracking-normal",
       "paragraph/Large Link":
